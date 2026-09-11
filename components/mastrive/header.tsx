b7cc7db1b7c2b0d46d7b54a53b7cc2abf856e85c
palx <2001palash@gmail.com>
2026-09-11 23:57:14 +0530

    fix: import useMemo in header to resolve client runtime crash on production

--- components/mastrive/header.tsx
+++ components/mastrive/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Wallet, User, Menu, X } from 'lucide-react'
